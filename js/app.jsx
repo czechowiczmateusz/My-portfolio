b7f098d5988c7395133906211bd6ed8c702a7c70
czechowiczmateusz <matic1.czechowicz@gmail.com>
2018-06-06 10:37:26 +0200

    3.4

--- js/app.jsx
+++ js/app.jsx
@@ -59,13 +59,22 @@ class App extends React.Component {
                         <h3>Projects</h3>
                         <hr width="40%" />
                         <h4>Film-search-app</h4>
-                        <a target="_blank" href="https://github.com/czechowiczmateusz/Film-search-app"><i className="fab fa-github fa-10x"></i></a>
+                        <div>
+                            <a target="_blank" href="https://czechowiczmateusz.github.io/Film-search-app/"><i className="fas fa-globe fa-10x"></i></a>
+                            <a target="_blank" href="https://github.com/czechowiczmateusz/Film-search-app"><i className="fab fa-github fa-10x"></i></a>
+                        </div>
                         <div data-aos="zoom-in" className="first_project"></div>
                         <h4>Tomorrowland-page</h4>
-                        <a target="_blank" href="https://github.com/czechowiczmateusz/Tomorrowland-page"><i className="fab fa-github fa-10x"></i></a>
+                        <div>
+                            <a target="_blank" href="https://czechowiczmateusz.github.io/Tomorrowland-page/"><i className="fas fa-globe fa-10x"></i></a>
+                            <a target="_blank" href="https://github.com/czechowiczmateusz/Tomorrowland-page"><i className="fab fa-github fa-10x"></i></a>
+                        </div>
                         <div data-aos="zoom-in" className="second_project"></div>
                         <h4>Sit-on-chair</h4>
-                        <a target="_blank" href="https://github.com/czechowiczmateusz/Sit-on-chair"><i className="fab fa-github fa-10x"></i></a>
+                        <div>
+                            <a target="_blank" href="https://czechowiczmateusz.github.io/Sit-on-chair/"><i className="fas fa-globe fa-10x"></i></a>
+                            <a target="_blank" href="https://github.com/czechowiczmateusz/Sit-on-chair"><i className="fab fa-github fa-10x"></i></a>
+                        </div>
                         <div data-aos="zoom-in" className="third_project"></div>
                     </div>
                 </section>
